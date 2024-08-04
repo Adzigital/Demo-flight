e5ebf2452af311c564eabf3f4138a3bd559b517b
dreamer-rgb <soumabhabiswas55@gmail.com>
2024-08-04 21:36:22 +0530

    fix:loading issue fixed

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,9 @@
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), react()],
   server: {
     port: 5500, 
   },
